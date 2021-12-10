76656185eb579b19908409a26a4b759e8300757c
Mao <mao8a87@gmail.com>
2021-12-10 08:38:22 -0500

    primer commit

---
+++ src/views/Tema2.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 2
+      h1 Título principal, 30pt
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema2',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
